8afad05bf561d37d593273c7fea77fbe27822af0
Armando Cerrillo <armando@cerrillomedia.org>
2016-12-07 18:44:51 -0800

    create seek-bar directive for click handling and slider dragging

---
+++ app/scripts/directives/seekBar.js
@@ -0,0 +1,62 @@
+(function() {
+    function seekBar($document) {
+        
+        var calculatePercent = function(seekBar, event) {
+            var offsetX = event.pageX - seekBar.offset().left;
+            var seekBarWidth = seekBar.width();
+            var offsetXPercent = offsetX / seekBarWidth;
+            offsetXPercent = Math.max(0, offsetXPercent);
+            offsetXPercent = Math.min(1, offsetXPercent);
+            return offsetXPercent;            
+        };
+        
+        return {
+            templateUrl: '/templates/directives/seek_bar.html',
+            replace: true,
+            restrict: 'E',
+            scope: { },
+            link: function(scope, element, attributes) {
+                scope.value = 0;
+                scope.max = 100;
+                
+                var seekBar = $(element); // adding jQuery 
+                
+                var percentString = function() {
+                    var value = scope.value;
+                    var max = scope.max;
+                    var percent = value / max * 100;
+                    return percent + "%";
+                };
+                
+                scope.fillStyle = function() {
+                    return {width: percentString()};
+                };
+                
+                // Click Handler
+                scope.onClickSeekBar = function(event) {
+                    var percent = calculatePercent(seekBar, event);
+                    scope.value = percent * scope.max;
+                };
+                
+                // attach mousemove to ng-mousedown
+                scope.trackThumb = function() {
+                    $document.bind('mousemove', function(event) {
+                        var percent = calculatePercent(seekBar, event);
+                        scope.$apply(function() {
+                            scope.value = percent * scope.max;
+                        });
+                    });
+                    
+                    $document.bind('mouseup.thumb', function() {
+                        $document.unbind('mousemove.thumb');
+                        $document.unbind('mouseup.thumb');
+                    });
+                };
+            }
+        };
+    }
+    
+    angular
+        .module('blocJams')
+        .directive('seekBar', ['$document', seekBar]);
+})();
